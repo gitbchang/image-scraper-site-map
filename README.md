4e6c199a3c9c5dd005de1f3a00fce0004ef06f16
gitbchang <brian.h.chang1@gmail.com>
2017-07-31 15:42:12 -0500

    xpath update

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
   * To add: write to file as stream.
 
 2. `pull_url.py` Use python script to pull all urls from the generated xml file
-  * NOT WORKING, should use a xml parser and XPATH query to pull all values
-  * Workaround - find and replace opening tags with blank strings
+  * Working - should use a xml parser and XPATH query to pull all values
+  * ~Workaround - find and replace opening tags with blank strings~
 
 3. In terminal, run `get_info.js` to scrape data from URL's gathered in step 2
   * Wrap all URL's into an array and feed it into the .forEach function
--- pull_url.py
+++ pull_url.py
@@ -1,22 +1,34 @@
-from lxml import etree as ET
+from lxml import etree
+from io import StringIO, BytesIO
 import csv
 
 filename = 'FINAL_justproducts2.xml'
 file = open(filename, encoding='utf-8')
-feed = ET.parse(file)
+feed = etree.parse(file)
 root = feed.getroot()
+client_instance = "sunextools"
+write_csv_url = []
+
+# insert namespace before each tag in xpath query
 namespace = '{' + root.nsmap[None] + '}'
-# print(namespace)
 
+all_urls = root.findall('.//' + namespace + 'url/' + namespace + 'loc')
 
+seeTree = etree.tostring(root)
+test = etree.tostring(all_urls[0])
 
-all_urls = root.find('.//' + namespace + 'url')
-# all_loc = all_urls.findall('.//' + namespace + 'loc')
-for loc in all_urls:
-  targetUrl = loc.find('.//' + namespace + 'loc')
-  print(targetUrl)
+for url in all_urls:
+  write_csv_url.append(url.text)
 
-for k, v in all_urls.items():
-  print(k, v)
+write_csv_name = client_instance + "extracted_urls.csv"
 
-# print(all_urls.items())
+"""
+If CSV already created, delete first
+
+Does not overwrite file - will append
+"""
+with open(write_csv_name, 'a') as resultFile:
+  for url in write_csv_url:
+    resultFile.write(url+"\n")
+    
+
