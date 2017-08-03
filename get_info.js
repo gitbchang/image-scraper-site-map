da9af510c9bc0a1eed56d2a4e11dcb3890d58d72
gitbchang <brian.h.chang1@gmail.com>
2017-08-03 08:44:16 -0500

    updates

--- get_info.js
+++ get_info.js
@@ -1,6 +1,9 @@
 // var axios = require('axios');
 var request = require('sync-request');
 var cheerio = require('cheerio');
+var fs = require('fs');
+var $ = jQuery = require('jquery');
+var json2csv = require('json2csv');
 var productPageUrls = require('./prod_url_array.js');
 
 var targetUrl = 'http://sunextools.com/product/43pc-12-dr-sae-master-impact-socket-set';
@@ -21,14 +24,42 @@ var testUrls = ['http://sunextools.com/product/22-ton-truck-axle-jack-w-air-retu
 'http://sunextools.com/product/2-ton-bottle-jack/']
 
 // console.log(productPageUrls); getProductDetails(targetUrl);
-getProductDetailsSync(targetUrl);
+// getProductDetailsSync(targetUrl);
 
-testUrls.forEach(function (url) {
-  // console.log(url) getProductDetails(url);
-  getProductDetailsSync(url);
+var datetime = Date.now();
+console.log(datetime);
+var csvFile = `sunex_test_data_${datetime}.csv`;
+
+var csvFields = ['ProductId', 'ProductName', 'Description', 'ImageUrl', 'ProductUrl'];
+// append data to this array, eventually write array to csv file
+var writeArray = []
+
+
+scrapeData(productPageUrls);
+
+function scrapeData(urlArray) {
+  // GET DATA FROM URLS
+  urlArray.forEach(function (url) {
+    // console.log(url) getProductDetails(url);
+    getProductDetailsSync(url);
+  });
+  // WRITE TO CSV
+  var csvData = json2csv({data: writeArray, fields: csvFields});
+
+  fs.writeFile(csvFile, csvData, function(err) {
+  if (err) throw err;
+  console.log('file saved');
 });
+  
+}
 
 function getProductDetailsSync(prodUrl) {
+  console.log("========================================================================================");
+  console.log("SCRAPING FROM:", prodUrl);
+  console.log("========================================================================================");
+  // append each object to the writeArray
+  var writeObject = {};
+  writeObject.ProductUrl = prodUrl;
   var res = request('GET', prodUrl);
   var siteHtml = res
     .body
@@ -42,19 +73,21 @@ function getProductDetailsSync(prodUrl) {
 
   prodHeader = $('.breadcrumb_last').text();
   console.log("prodHeader", prodHeader);
+  writeObject.ProductName = prodHeader;
 
   var descriptionArray = [];
   $('.hill_product_description ul').each(function (index, elem) {
     descriptionArray[index] = $(this).text();
   });
-
   prodDescription = descriptionArray.join(' ');
   console.log('prodDescription', prodDescription);
+  writeObject.Description = prodDescription;
 
   prodImageUrl = $('a')
     .filter('.woocommerce-main-image')
     .attr('href');
   console.log('image url', prodImageUrl);
+  writeObject.ImageUrl = prodImageUrl;
 
   // MODEL NUMBER could not find a good way to target just model number, pulling
   // the h1 tag and the header without id. then stripping out the header to have
@@ -63,15 +96,25 @@ function getProductDetailsSync(prodUrl) {
   stripHeader = ` - ${prodHeader}`;
   var prodId = fullHeader.replace(stripHeader, "");
   console.log('prodId', prodId);
+  writeObject.ProductId = prodId;
+
+  writeArray.push(writeObject);
 
   // returnObject.pNumber = prodNumber; returnObject.pHeader = prodHeader;
   // returnObject.pDescription = prodDescription; returnObject.pImageUrl =
   // prodImageUrl; return returnObject;
 
+  
+
 }
+
+
+
+
 // ****************************************
 // DO NOT USE THE ASYNC - WILL BREAK SERVER
 // ****************************************
+// Can use async if using a setInterval() or add an interval to ASYNC/AWAIT loop
 /*
 
 function getProductDetails(prodUrl) {
